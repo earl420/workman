eb7b49876407d4c0f2f8075cfb6fc864acbc1d33
saranghailey <sr952412@hanmail.net>
2019-10-14 13:22:14 +0900

    11

--- src/main/java/com/wework/workman/calendar/model/vo/Calendar.java
+++ src/main/java/com/wework/workman/calendar/model/vo/Calendar.java
@@ -1,5 +1,150 @@
 package com.wework.workman.calendar.model.vo;
 
+import java.sql.Date;
+
 public class Calendar {
+	
+	private String _id;
+	private String title;
+	private String description;
+	private Date start;
+	private Date end;
+	private String type;
+	private String userName;
+	private String backgroundColor;
+	private String textColor;
+	private Boolean allDay;
+	
+	
+	public Calendar() {
+		super();
+	}
+
+
+	public Calendar(String _id, String title, String description, Date start, Date end, String type, String userName,
+			String backgroundColor, String textColor, Boolean allDay) {
+		super();
+		this._id = _id;
+		this.title = title;
+		this.description = description;
+		this.start = start;
+		this.end = end;
+		this.type = type;
+		this.userName = userName;
+		this.backgroundColor = backgroundColor;
+		this.textColor = textColor;
+		this.allDay = allDay;
+	}
+
+
+	public String get_id() {
+		return _id;
+	}
+
+
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public Date getStart() {
+		return start;
+	}
+
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+
+	public Date getEnd() {
+		return end;
+	}
+
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+	public String getBackgroundColor() {
+		return backgroundColor;
+	}
+
+
+	public void setBackgroundColor(String backgroundColor) {
+		this.backgroundColor = backgroundColor;
+	}
+
+
+	public String getTextColor() {
+		return textColor;
+	}
+
+
+	public void setTextColor(String textColor) {
+		this.textColor = textColor;
+	}
+
+
+	public Boolean getAllDay() {
+		return allDay;
+	}
+
+
+	public void setAllDay(Boolean allDay) {
+		this.allDay = allDay;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Calendar [_id=" + _id + ", title=" + title + ", description=" + description + ", start=" + start
+				+ ", end=" + end + ", type=" + type + ", userName=" + userName + ", backgroundColor=" + backgroundColor
+				+ ", textColor=" + textColor + ", allDay=" + allDay + "]";
+	}
+	
+	
+	
 
 }
--- src/main/webapp/WEB-INF/views/calendar/NewFile.jsp
+++ src/main/webapp/WEB-INF/views/calendar/NewFile.jsp
@@ -1,13 +1,96 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+<meta charset='utf-8' />
+
+
+  <title>
+   	Calendar
+  </title>
+
+
+<link href='/assets/demo-to-codepen.css' rel='stylesheet' />
+
+
+  <style>
+
+    html, body {
+      margin: 0;
+      padding: 0;
+      font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+      font-size: 14px;
+    }
+
+    #calendar {
+      max-width: 900px;
+      margin: 40px auto;
+    }
+
+  </style>
+
+
+<link href='https://unpkg.com/@fullcalendar/core@4.3.1/main.min.css' rel='stylesheet' />
+
+
+  <link href='https://unpkg.com/@fullcalendar/daygrid@4.3.0/main.min.css' rel='stylesheet' />
+
+  <link href='https://unpkg.com/@fullcalendar/timegrid@4.3.0/main.min.css' rel='stylesheet' />
+
+  <link href='https://unpkg.com/@fullcalendar/list@4.3.0/main.min.css' rel='stylesheet' />
+
+  <link href='https://unpkg.com/@fullcalendar/bootstrap@4.3.0/main.min.css' rel='stylesheet' />
+
+
+<script src='/assets/demo-to-codepen.js'></script>
+
+<script src='https://unpkg.com/@fullcalendar/core@4.3.1/main.min.js'></script>
+
+
+
+
+  <script src='https://unpkg.com/@fullcalendar/daygrid@4.3.0/main.min.js'></script>
+
+  <script src='https://unpkg.com/@fullcalendar/timegrid@4.3.0/main.min.js'></script>
+
+  <script src='https://unpkg.com/@fullcalendar/list@4.3.0/main.min.js'></script>
+
+  <script src='https://unpkg.com/@fullcalendar/bootstrap@4.3.0/main.min.js'></script>
+
+
+
+  <link href='https://use.fontawesome.com/releases/v5.0.6/css/all.css' rel='stylesheet'>
+<link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' rel='stylesheet' />
+<script>
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      plugins: [ 'dayGrid', 'timeGrid', 'list', 'bootstrap' ],
+      timeZone: 'UTC',
+      themeSystem: 'bootstrap',
+      header: {
+        left: 'prev,next',
+        center: 'title',
+        right: 'today'
+      },
+      eventLimit: true, // allow "more" link when too many events
+      events: 'https://fullcalendar.io/demo-events.json'
+    });
+
+    calendar.render();
+  });
+
+</script>
+
 </head>
 <body>
-
+  
+  <div id='calendar'></div>
 </body>
+
 </html>
