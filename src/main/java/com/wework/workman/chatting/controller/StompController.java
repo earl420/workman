e564a22f6668ea642a9225f79f10f4efec62b560
SCzedge <sczedge@gmail.com>
2019-10-16 21:54:04 +0900

    20191016 21:53

--- src/main/java/com/wework/workman/chatting/controller/StompController.java
+++ src/main/java/com/wework/workman/chatting/controller/StompController.java
@@ -1,32 +1,32 @@
-package com.wework.workman.chatting.controller;
-
-import com.wework.workman.chatting.model.vo.ChatMessage;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.RestController;
-
-
-
-@RestController
-public class StompController {
-
-    private final SimpMessagingTemplate template;
-
-    @Autowired
-    public StompController(SimpMessagingTemplate template) {
-        this.template = template;
-    }
-
-    @MessageMapping("/chat/join")
-    public void join(ChatMessage message) {
-        message.setMessage(message.getWriter() + "¥‘¿Ã ¿‘¿Â«œºÃΩ¿¥œ¥Ÿ.");
-        template.convertAndSend("/subscribe/chat/room/" + message.getChatRoomId(), message);
-    }
-
-    @MessageMapping("/chat/message")
-    public void message(ChatMessage message) {
-        template.convertAndSend("/subscribe/chat/room/" + message.getChatRoomId(), message);
-    }
-}
+//package com.wework.workman.chatting.controller;
+//
+//import com.wework.workman.chatting.model.vo.ChatMessage;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.simp.SimpMessagingTemplate;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//
+//
+//@RestController
+//public class StompController {
+//
+//    private final SimpMessagingTemplate template;
+//
+//    @Autowired
+//    public StompController(SimpMessagingTemplate template) {
+//        this.template = template;
+//    }
+//
+//    @MessageMapping("/chat/join")
+//    public void join(ChatMessage message) {
+//        message.setMessage(message.getWriter() + "¥‘¿Ã ¿‘¿Â«œºÃΩ¿¥œ¥Ÿ.");
+//        template.convertAndSend("/subscribe/chat/room/" + message.getChatRoomId(), message);
+//    }
+//
+//    @MessageMapping("/chat/message")
+//    public void message(ChatMessage message) {
+//        template.convertAndSend("/subscribe/chat/room/" + message.getChatRoomId(), message);
+//    }
+//}
