a07340824f41f874941c403f81ef07fcff6966a0
MinByungHyun <53996116+MinByungHyun@users.noreply.github.com>
2019-10-16 12:24:09 +0900

    민병현 2019-10-16

--- src/main/java/com/wework/workman/chatting/model/vo/ChatMessage.java
+++ src/main/java/com/wework/workman/chatting/model/vo/ChatMessage.java
@@ -1,5 +1,7 @@
 package com.wework.workman.chatting.model.vo;
 
+import java.awt.TrayIcon.MessageType;
+
 public class ChatMessage {
 	private String chatRoomId;
 	private String writer;
--- src/main/java/com/wework/workman/mypage/model/service/MypageServiceImpl.java
+++ src/main/java/com/wework/workman/mypage/model/service/MypageServiceImpl.java
@@ -2,23 +2,27 @@ package com.wework.workman.mypage.model.service;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wework.workman.hunamResource.model.vo.HumanResource;
 import com.wework.workman.mypage.model.dao.MypageDao;
+import com.wework.workman.mypage.model.vo.Employee;
 
 @Service("mypageService")
 public class MypageServiceImpl implements MypageService{
 	
 	
-	@Resource(name="mypageDao")
+	@Autowired
 	private MypageDao mDao;
 
 	/**
 	 * 로그인
 	 */
+
 	@Override
-	public HumanResource loginMan(HumanResource m) {
-		return mDao.loginMan(m);
+	public HumanResource loginMan(Employee m) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
