976a870ed13814786b51fe0a3dc7aede7c67832d
kj1kj2kj3 <kjhstan@gmail.com>
2019-10-18 14:54:33 +0900

    1111

--- src/main/java/com/wework/workman/mypage/controller/MypageController.java
+++ src/main/java/com/wework/workman/mypage/controller/MypageController.java
@@ -1,21 +1,24 @@
 package com.wework.workman.mypage.controller;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.wework.workman.mypage.model.service.MypageService;
 import com.wework.workman.mypage.model.vo.Mypage;
 
-@SessionAttributes("")
+@SessionAttributes("loginMan")
 @Controller
 public class MypageController {
 
@@ -103,23 +106,28 @@ public class MypageController {
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping(value = "login.do", method = RequestMethod.POST)
-	public String loginEmp(Mypage m, Model model) {
 
-		Mypage loginMan = mService.loginMan(m);
-		
-		logger.debug(loginMan.toString());
-		
-		if(loginMan != null && bcryptPasswordEncoder.matches(m.getPwd(), loginMan.getPwd())) {
-			model.addAttribute("loginMan", loginMan);
-			return "redirect:home.wo";
-			
-		}else {
-			
-			return "redirect:loginPage.wo";
-		}
-		
-	}
+	  @RequestMapping(value = "login.wo", method = RequestMethod.POST) 
+	  public String loginMan(Mypage m, Model model) {
+	  
+		  Mypage loginMan = mService.loginMan(m);
+		  
+		  System.out.println(m.getNum());
+		  
+		  if(loginMan != null && bcryptPasswordEncoder.matches(m.getPwd(), loginMan.getPwd())) {
+			  
+			  model.addAttribute("loginMan", loginMan); 
+			  
+			  return "redirect:home.wo";
+		  
+		  }else {
+			  
+			  return "redirect:loginPage.wo";
+	  
+		  }
+	  
+	  }
+	 
 
 	/**
 	 * 로그아웃
--- src/main/java/com/wework/workman/mypage/model/dao/MypageDao.java
+++ src/main/java/com/wework/workman/mypage/model/dao/MypageDao.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import com.wework.workman.mypage.model.vo.Mypage;
 
 @Repository("mypageDao")
+
 public class MypageDao {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
