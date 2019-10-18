e65fbd20ac29c5012ea033c9ace3e5d32c2de5fc
saranghailey <sr952412@hanmail.net>
2019-10-18 14:33:16 +0900

    힘들당

--- src/main/java/com/wework/workman/calendar/model/dao/CalendarDao.java
+++ src/main/java/com/wework/workman/calendar/model/dao/CalendarDao.java
@@ -13,22 +13,22 @@ public class CalendarDao {
 	
 	// 일정등록
 	public int insertCalendar(Calendar c) {
-		return sqlSession.insert("calendarMapper.insertCalendar", c);
+		return sqlSession.insert("calendar-mapper.insertCalendar", c);
 	}
 	
 	// 일정수정
 	public int updateCalendar(int _id) {
-		return sqlSession.update("calendarMapper.updateCalendar", _id);
+		return sqlSession.update("calendar-mapper.updateCalendar", _id);
 	}
 	
 	// 일정상세보기
 	public Calendar CalendarDetail(int _id) {
-		return sqlSession.selectOne("calendarMapper.CalendarDetail", _id);
+		return sqlSession.selectOne("calendar-mapper.CalendarDetail", _id);
 	}
 	
 	// 일정삭제
 	public int deleteCalendar(int _id) {
-		return sqlSession.update("calendarMapper.deleteCalendar", _id);
+		return sqlSession.update("calendar-mapper.deleteCalendar", _id);
 	}
 }
 
