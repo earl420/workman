008ba7e4a6f1e7d46213750efbedd127837af29b
earl420 <jenius420@daum.net>
2019-10-24 10:18:13 +0900

    11

--- src/main/java/com/wework/workman/mypage/model/service/MypageServiceImpl.java
+++ src/main/java/com/wework/workman/mypage/model/service/MypageServiceImpl.java
@@ -5,7 +5,7 @@ import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.wework.workman.hunamResource.model.vo.HumanResource;
+//import com.wework.workman.hunamResource.model.vo.HumanResource;
 import com.wework.workman.mypage.model.dao.MypageDao;
 import com.wework.workman.mypage.model.vo.Mypage;
 
