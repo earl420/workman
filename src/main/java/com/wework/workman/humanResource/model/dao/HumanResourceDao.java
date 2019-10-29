ac408733ef5d5e5b1e0f3ecc5ae2201a5814a626
YEONJAE-KANG <53990445+YEONJAE-KANG@users.noreply.github.com>
2019-10-29 14:26:27 +0900

    yeonjae

--- src/main/java/com/wework/workman/humanResource/model/dao/HumanResourceDao.java
+++ src/main/java/com/wework/workman/humanResource/model/dao/HumanResourceDao.java
@@ -17,48 +17,73 @@ import com.wework.workman.humanResource.model.vo.Notice;
 public class HumanResourceDao {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
-	
-	public int gethListCount() {
-		
-		return sqlSession.selectOne("humanMapper.gethListCount");
+
+	public int getnListCount() {
+
+		return sqlSession.selectOne("humanMapper.getnListCount");
 	}
-	
-	public ArrayList<Notice> selectHRnList(PageInfo pi){
-		
+
+	public ArrayList<Notice> selectnList(PageInfo pi) {
+
 		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
 		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		
-		return (ArrayList)sqlSession.selectList("humanMapper.selectHRnList", null, rowBounds);
-		
+
+		return (ArrayList) sqlSession.selectList("humanMapper.selectnList", null, rowBounds);
+
 	}
 	
-	public Notice gethrnDetail(String noticeNum) {
-		
+	public Notice getnDetail(String noticeNum) {
+
 		return sqlSession.selectOne("humanMapper.selectNotice", noticeNum);
 	}
 	
-	public int insertNotice(Notice n) {
+	public int updateNotice(Notice n) {
 		
+		return sqlSession.update("humanMapper.updateNotice", n);
+	}
+
+
+	public int gethListCount() {
+
+		return sqlSession.selectOne("humanMapper.gethListCount");
+	}
+
+	public ArrayList<Notice> selectHRnList(PageInfo pi) {
+
+		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
+		RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+
+		return (ArrayList) sqlSession.selectList("humanMapper.selectHRnList", null, rowBounds);
+
+	}
+
+	public Notice gethrnDetail(String noticeNum) {
+
+		return sqlSession.selectOne("humanMapper.selectNotice", noticeNum);
+	}
+
+	public int insertNotice(Notice n) {
+
 		return sqlSession.insert("humanMapper.insertNotice", n);
 	}
-	
+
 	public int insertAtt(Attachment a) {
-		
+
 		return sqlSession.insert("humanMapper.insertAtt", a);
 	}
-	
-	public ArrayList<Department> selectDeptList(){
-		
-		return (ArrayList)sqlSession.selectList("humanMapper.selectDeptList");
+
+	public ArrayList<Department> selectDeptList() {
+
+		return (ArrayList) sqlSession.selectList("humanMapper.selectDeptList");
 	}
-	
-	public ArrayList<Employee> selectEmp(){
-		
-		return (ArrayList)sqlSession.selectList("humanMapper.selectEmp");
+
+	public ArrayList<Employee> selectEmp() {
+
+		return (ArrayList) sqlSession.selectList("humanMapper.selectEmp");
 	}
-	
+
 	public ArrayList<Employee> getEmpCount() {
-		
-		return (ArrayList)sqlSession.selectList("humanMapper.selectEmpCount");
+
+		return (ArrayList) sqlSession.selectList("humanMapper.selectEmpCount");
 	}
 }
